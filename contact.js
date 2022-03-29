4112a445a19adf29b5f6609ef78458b16cdfa6d5
Bram00943 <bramnieuwland12@gmail.com>
2022-03-29 22:25:46 +0200

    d

--- contact.js
+++ contact.js
@@ -1,5 +1,41 @@
-$('button').click(function(){
-  $('button').toggleClass('active');
-  $('.title').toggleClass('active');
-  $('nav').toggleClass('active');
-});
+
+
+var form = document.getElementById("my-form");
+
+function succes(){
+  var status = document.getElementById("status");
+  form.reset();
+  status.innerHTML = "thanks";
+}
+function error(){
+  var status = document.getElementById("status");
+  status.innerHTML = "Oops! There was an problem.";
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
