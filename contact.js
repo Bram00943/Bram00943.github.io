c6a98f5eef49daf18421757f7e2c753627ac0b06
Bram00943 <bramnieuwland12@gmail.com>
2022-03-29 12:57:25 +0200

    big edits

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
 $('button').click(function(){
-    $('button').toggleClass('active');
-    $('.title').toggleClass('active');
-    $('nav').toggleClass('active');
-  });
+  $('button').toggleClass('active');
+  $('.title').toggleClass('active');
+  $('nav').toggleClass('active');
+});
