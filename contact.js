65983caf1d27591e09d0b36a9ff0acd8bd0e731c
Bram00943 <bramnieuwland12@gmail.com>
2022-03-31 12:29:51 +0200

    j

--- contact.js
+++ contact.js
@@ -1,41 +1,41 @@
+window.addEventListener("DOMContentLoaded", function(){
 
 
-var form = document.getElementById("my-form");
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("status")
 
-function succes(){
-  var status = document.getElementById("status");
-  form.reset();
-  status.innerHTML = "thanks";
-}
-function error(){
-  var status = document.getElementById("status");
-  status.innerHTML = "Oops! There was an problem.";
-}
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = "Thanks for your submission!";
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "Oops! There was a problem submitting your form"
-        }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
+  function succes(){
+    form.reset();
+    status.classList.add('succes')
+    status.innerHTML = "Thanks!";
+  }
+  function error(){
+    status.classList.add('error')
+    status.innerHTML = "Oops! There was an problem.";
+  }
+
+  form.addEventListener("submit", function (ev){
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, succes, error)
   });
+});
+
+function ajax(method, url, data, succes, error){
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState !== XMLHttpRequest.DONE) return;
+    if(xhr.status === 200){
+      succes(xhr.response, xhr.responseType);
+    }else{
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
+
+
+
